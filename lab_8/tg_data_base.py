0538afe15c5c7a4bb321de341bef5a33fa910748
freegotohell <90061474+freegotohell@users.noreply.github.com>
2025-12-05 17:03:34 +0400

    add import for tg_notif

---
+++ lab_8/tg_data_base.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+
+conn = sqlite3.connect('users.db', check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER PRIMARY KEY,
+    chat_id TEXT UNIQUE
+);
+''')
+conn.commit()
+
+__all__ = ['conn', 'cursor']
