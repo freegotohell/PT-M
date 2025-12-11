89a7c255b58bfb5ab53c6a133fcba079bf8de0d3
freegotohell <90061474+freegotohell@users.noreply.github.com>
2025-12-12 03:11:01 +0400

    Move files to target folder

--- lab_6/crypto_system.py
+++ lab_6/crypto_system.py
@@ -1,7 +1,7 @@
-from cryptography.hazmat.primitives import padding, hashes
+from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 
-from asymmetrical import AsymmetricCryptography
+from lab_6.asymmetrical import AsymmetricCryptography
 from serialize import Serialization
 from symmetrical import SymmetricCryptography
 from work_file import *
